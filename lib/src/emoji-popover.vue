cedf80412889258b5c665cc225a468c631f9e859
hackycy <qa894178522@qq.com>
2021-08-31 17:29:56 +0800

    feat：define props

---
+++ lib/src/emoji-popover.vue
@@ -0,0 +1,73 @@
+<template>
+  <transition :name="transition">
+    <!-- -->
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'EmojiPopover',
+  props: {
+    emojis: {
+      type: Array,
+      required: true
+    },
+    width: {
+      type: Number,
+      default: 410
+    },
+    contentHeight: {
+      type: Number,
+      default: 270
+    },
+    size: {
+      type: Number,
+      default: 28
+    },
+    gutter: {
+      type:  [Number, Array],
+      default: 10
+    },
+    unit: {
+      type: String,
+      default: 'px'
+    },
+    recentlyUsed: {
+      type: Boolean,
+      default: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    transition: {
+      type: String,
+      default: 'ep-fade-in-linear'
+    },
+    emojiClass: {
+      type: String,
+      default: null
+    },
+    placement: {
+      type: String,
+      default: 'top'
+    },
+    appendToBody: {
+      type: Boolean,
+      default: true
+    },
+    offset: {
+      type: Number,
+      default: 0
+    },
+    visibleArrow: {
+      type: Boolean,
+      default: true
+    },
+    arrowSize: {
+      type: Number,
+      default: 8
+    }
+  }
+}
+</script>
