ea87f84d237932d12642ad0a64bfe74fc1746bd8
hackycy <qa894178522@qq.com>
2021-08-31 12:04:48 +0800

    chore：update script

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 'use strict'
 
-module.exports = {}
+const name = require('./package.json').name
+
+module.exports = {
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'examples/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      title: `${name} Demo`
+    }
+  }
+}
